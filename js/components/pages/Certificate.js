a2594245773e951d162fa78cb397e8236ff9c0d8
Vivien Tang <vivientang@deloitte.co.uk>
2020-02-10 22:38:48 +0000

    partial implementation of certificate page

--- js/components/pages/Certificate.js
+++ js/components/pages/Certificate.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import { withRouter } from 'react-router'
+import headerLogo from '../../../assets/images/headerlogo.jpg'
 
 const Certificate = () => (
-    <div>Certificate</div>
+    <div className="certWidth">
+        <div className="certContent">
+            <div className="certWrapper">
+                <div className="certBackgroundWrapper">
+                    <div className="certPreviewOverlay">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 )
 
 export default withRouter(Certificate)
